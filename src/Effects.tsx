f43c748918c48702c4a07fd21d387abddcef27fb
dwoichenkoDmitry <66391940+dwoichenkoDmitry@users.noreply.github.com>
2022-05-25 22:16:49 +0500

    task-react-render-and-effects

--- src/Effects.tsx
+++ src/Effects.tsx
@@ -1,5 +1,26 @@
 import { subscribe, unsubscribe } from './resources/API';
+import { useEffect, useState } from 'react';
 
 export function Effects(props: { sourceId: string }) {
-    return <div>123</div>;
+    const [mes, setMes] = useState(-1);
+
+    function call(message: number) {
+        setMes(message);
+    }
+
+    useEffect(() => {
+        subscribe(props.sourceId, call);
+        return () => {
+            unsubscribe(props.sourceId, call);
+            setMes(-1);
+        };
+    }, [props.sourceId]);
+
+    return (
+        <div>
+            <p>
+                {props.sourceId}: {mes}
+            </p>
+        </div>
+    );
 }
